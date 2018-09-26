6146910daaec822418aa9f271591ac5a3ea2ccfa
Rikako Yamamoto <yamamotorikako27@gmail.com>
2018-09-26 09:05:35 -0700

    fix resultList function

--- nasa_hydrology.py
+++ nasa_hydrology.py
@@ -6,7 +6,7 @@ import sys
 from tabulate import tabulate
 
 url = " http://rapid-hub.org/data/angles_UCI_CS.csv"
-savefile = "output.csv"
+savefile = "angles_UCI_CS.csv"
 
 
 def resultList(filename):
@@ -14,13 +14,11 @@ def resultList(filename):
 #whose keys are "station_id", "angle_degrees", and "cosine"
     l = []
     with open(filename) as csv_file:
-        reader = csv.DictReader(csv_file, skipinitialspace=True)
-        for i in reader:
-            l.append(i)
-    
+        l = list(csv.DictReader(csv_file, skipinitialspace=True))
+
     for i in l:
         i.update({'cosine': math.cos(math.radians(eval(i['angle_degrees'])))})
-    
+#     print(l)
     return l
 
 def formattedTable(d):
@@ -35,12 +33,12 @@ if __name__ == '__main__':
         #fetching URLs
         res = urllib.request.urlopen(url)
         data = res.read()
-        
+
         #write the data to "output.csv"
         with open(savefile, mode='wb') as f:
             f.write(data)
         
-        print(data)
+#         print(data)
         
         result = resultList(savefile)
         print(formattedTable(result))
